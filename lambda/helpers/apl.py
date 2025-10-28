32c88f230177df30ff1abb5093e4d364fee54949
Christian Mendieta <christian.mendieta.t@gmail.com>
2025-10-28 08:48:19 -0500

    Corrigiendo APL.

--- lambda/helpers/apl.py
+++ lambda/helpers/apl.py
@@ -3,10 +3,14 @@ from ask_sdk_model.interfaces.alexa.presentation.apl import RenderDocumentDirect
 import os
 import json
 
+from helpers.dev import insertar_error
+import traceback
+
 
 def get_apl_directive(apl_docname, datasources, token):
     try:
-        apl_path = os.path.join(os.path.dirname(__file__), "apldocs", apl_docname)
+        base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "apldocs"))
+        apl_path = os.path.join(base_path, apl_docname)
 
         with open(apl_path, "r", encoding="utf-8") as apl_file:
             apl_document = json.load(apl_file)
@@ -18,6 +22,6 @@ def get_apl_directive(apl_docname, datasources, token):
         )
 
     except Exception:
-        from helpers.dev import insertar_error
-        insertar_error()
+        error_trace = traceback.format_exc()
+        insertar_error(error_trace)
         return None
